a500827a0509a280a3e783c0bc90d9602b035963
Afan Riaz <afanriaz202@gmail.com>
2026-01-12 14:37:29 +0500

    Task 2 completed

---
+++ Task_2/task_2_app/README.md
@@ -0,0 +1,36 @@
+# task_2_app
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+### 1. Create new Flutter project 
+
+![Screenshot](assets/1.jpg)
+
+### 2. Create ProfileCard class extending StatelessWidget 
+### 3. Use Container with BoxDecoration 
+### 4. Add borderRadius and boxShadow 
+### 5. Use Column for vertical layout 
+
+![Screenshot](assets/5.jpg)
+
+### 6. Add CircleAvatar with NetworkImage 
+### 7. Add Text widgets with custom TextStyle 
+
+![Screenshot](assets/7.jpg)
+
+### 8. Use Row for social media icons 
+### 9. Add Icon widgets 
+### 10. Implement Padding and SizedBox for spacing 
+
+![Screenshot](assets/10.jpg)
+
+### 11. Create separate widget classes for sections
+### 12. Run and screenshot UI 
+
+![Screenshot](assets/12.jpg)
---
+++ Task_2/task_2_app/lib/main.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+/* =======================
+   MAIN APP
+======================= */
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
+
+/* =======================
+   HOME SCREEN
+======================= */
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      body: const Center(child: ProfileCard()),
+    );
+  }
+}
+
+/* =======================
+   PROFILE CARD
+======================= */
+class ProfileCard extends StatelessWidget {
+  const ProfileCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 300,
+      padding: const EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.black26,
+            blurRadius: 10,
+            offset: Offset(0, 5),
+          ),
+        ],
+      ),
+      child: const Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          ProfileImage(),
+          SizedBox(height: 15),
+          ProfileName(),
+          SizedBox(height: 5),
+          ProfileRole(),
+          SizedBox(height: 15),
+          SocialIcons(),
+        ],
+      ),
+    );
+  }
+}
+
+/* =======================
+   PROFILE IMAGE
+======================= */
+class ProfileImage extends StatelessWidget {
+  const ProfileImage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CircleAvatar(
+      radius: 45,
+      backgroundImage: NetworkImage("assets/profile.jpg"),
+    );
+  }
+}
+
+/* =======================
+   PROFILE NAME
+======================= */
+class ProfileName extends StatelessWidget {
+  const ProfileName({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      "Afan Riaz",
+      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+    );
+  }
+}
+
+/* =======================
+   PROFILE ROLE
+======================= */
+class ProfileRole extends StatelessWidget {
+  const ProfileRole({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      "Flutter Intern",
+      style: TextStyle(fontSize: 14, color: Colors.grey),
+    );
+  }
+}
+
+/* =======================
+   SOCIAL ICONS
+======================= */
+class SocialIcons extends StatelessWidget {
+  const SocialIcons({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.only(top: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(Icons.facebook, color: Colors.blue),
+          SizedBox(width: 15),
+          Icon(Icons.camera_alt, color: Colors.purple),
+          SizedBox(width: 15),
+          Icon(Icons.email, color: Colors.red),
+        ],
+      ),
+    );
+  }
+}
